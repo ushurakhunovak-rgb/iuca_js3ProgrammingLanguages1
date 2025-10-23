5637933f25526b73363ecb5f7cfb5fee0c22d3ca
narmuhamedov <kingradomir@gmail.com>
2025-10-24 02:55:52 +0600

    tabsslider

--- js/lesson.js
+++ js/lesson.js
@@ -18,3 +18,68 @@ phoneButton.addEventListener('click', ()=>{
 })
 
 
+//TAB SLIDER
+const tabsContentCards = document.querySelectorAll('.tab_content_block');
+const tabsItems = document.querySelectorAll('.tab_content_item');
+const tabsItemsParents =  document.querySelector('.tab_content_items');
+
+
+const hightTabsContentCards = () =>{
+    tabsContentCards.forEach((tabsContentCard)=>{
+        tabsContentCard.style.display = 'none'
+    })
+    tabsItems.forEach((tabItem)=>{
+        tabItem.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabsContentCards = (indexElement = 0)=>{
+    tabsContentCards[indexElement].style.display = 'block';
+    tabsItems[indexElement].classList.add('tab_content_item_active')
+}
+
+hightTabsContentCards();
+showTabsContentCards();
+
+
+tabsItemsParents.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabsItems.forEach((tabItem, tabItemIndex)=>{
+            if(event.target === tabItem){
+                hightTabsContentCards()
+                showTabsContentCards(tabItemIndex)
+            }
+        })
+    }
+}
+
+let curretIndex = 0; // Первая вкладка
+let intervalId; //Переменная для хранения интервала
+
+//Ф-ция для автоматического переключения
+
+const startAuthoSlider = ()=>{
+    intervalId = setInterval(()=>{
+        hightTabsContentCards();
+        showTabsContentCards(curretIndex);
+        curretIndex = (curretIndex +1) % tabsItems.length;
+    }, 2000); // 2сек
+}
+//Запуск автослайдера
+startAuthoSlider();
+
+//Остановка слайдера при клике на вкладку
+
+tabsItemsParents.onclick = (event) => {
+    clearInterval(intervalId);
+    if (event.target.classList.contains('tab_content_item')){
+        tabsItems.forEach((tabItem, tabItemIndex) =>{
+            if(event.target === tabItem){
+                hightTabsContentCards();
+                showTabsContentCards(tabItemIndex);
+                curretIndex = tabItemIndex;
+                startAuthoSlider();
+            }
+        })
+    }
+}
